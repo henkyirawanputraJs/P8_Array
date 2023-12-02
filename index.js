c3eb06e1dd64bf2beaa2df943c23b1bf90057c95
henkyirawanputraJs <henkyirawanputra6@gmail.com>
2023-12-01 21:28:14 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// let makanan = ['sate', 'mie', 'roti']
+// console.log(makanan[1])
+
+// var hari = 'senin'
+// var hari = 'selasa'
+// console.log(hari)
+
+// let mahasiswa = ['dona','dina','duna','dena','daina','dara','dora']
+// console.log(mahasiswa[4])
+
+// let alamat= []
+// alamat = ['padang','jambi','bukittinggi']
+// console.log(alamat[1])
+
+// let nama = 'Henky'
+// console.log(nama.length)
+
+// let mahasiswa2 = ['andre','dila','fika','dola','sifa','siti',['budi','boda','bini','bola']]
+// console.log( mahasiswa2[6][2])
+
+// let mahasiswa3 = ['andre','dila',['cul','cil','cal',]]
+// console.log( mahasiswa3[2][2])
+
+
+// var arr = []
+// arr[0]='senin'
+// arr[1]='selasa'
+// arr[2]='rabu'
+// arr[3]='kamis'
+// console.log(arr)
+
+//let keluarga = ['andre','dila',['cul','cil','cal',]]
+//console.log( mahasiswa3[2][2])
+
+var arr2 = ['senin',,'rabu']
+console.log(arr2[1])
+
+var arr3 = ['senin','selasa','rabu']
+for (i=0; i<3; i++){
+    console.log(arr3[i])
+}
+
+let keluarga = ['suhu','mihi','aci','acu','aca','ace','aco'['budi','buda','budo','bude'['cul','cil','cal']]]
+console.log(keluarga[7][4][1])
+console.log('seorang kepala keluarga yang bernama ${keluarga[0]} memiliki istri bernama ${keluarga[1]}, ${keluarga[1]} ini memiliki 5 anak${keluarga[2]} anak yang bernama ${keluarga[2]} juga memiliki 4 anak ${keluarga[2][5]} anak yang bernama ${keluarga[2][5][0]} memiliki 3 orang anak yang bernama ${keluarga[2][5][4]}')
